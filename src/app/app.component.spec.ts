456c3990d357e0516df46b8d848ca492764d7979
Greg DiCristofaro <gregdicristofaro@gmail.com>
2026-04-25 22:31:02 -0400

    test fixes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,6 +18,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Constant Q Transform');
+    expect(compiled.querySelector('h2')?.textContent).toContain('Constant Q Transform');
   });
 });
